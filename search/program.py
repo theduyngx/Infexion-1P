ce9f91806445b13a640abed4a83ca8a06b4d5e0f
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-03-18 21:44:03 +1100

    Added moving pieces functions

--- search/program.py
+++ search/program.py
@@ -3,59 +3,171 @@
 
 from utils import render_board
 
-# # constants
-# SIZE = 7
-# MAX_PTS = SIZE ** 2
-#
-#
-# # get all possible roots
-# def get_all_roots(state: dict[tuple, tuple]) -> list[tuple]:
-#     total_score = sum(i for i, j in state.values())
-#     all_roots = []
-#     if total_score < MAX_PTS:
-#         for x in range(0, 6):
-#             for y in range(0, 6):
-#                 if (x, y) in state:
-#                     if state[(x, y)][0] == 'b':
-#                         continue
-#                 all_roots.append((x, y))
-#     else:
-#         for x, y in state.keys():
-#             if state[(x, y)][0] == 'r':
-#                 all_roots.append((x, y))
-#     return all_roots
+# constants
+SIZE = 7
+MAX_PTS = SIZE ** 2
+INF = 9999
+
+
+def get_all_roots(state: dict[tuple, tuple]) -> list[tuple]:
+    """
+    Get all roots that player can start with, given only SPREAD action is permitted.
+
+    Arguments:
+    state -- The provided state of the board.
+
+    Output:
+    a list of (x, y) positions of all possible start move (root).
+    """
+    all_roots = []
+    total_score = 0
+    for x, y in state.keys():
+        value = state[(x, y)]
+        total_score += value[1]
+        if value[0] == 'r':
+            all_roots.append((x, y))
+    if total_score > MAX_PTS or total_score <= 0:
+        return []
+    return all_roots
+
+def dfs_limited(state: dict[tuple, tuple], root: tuple, depth: int) -> tuple:
+# def dfs_limited(state: dict[tuple, tuple], root: tuple, depth: int) -> ([tuple], int, bool):
+    """
+    Depth-first limited search algorithm used for IDS. Searching up till a certain specified depth.
+
+    Arguments:
+    state -- The provided state of the board.
+    root  -- Start position.
+    depth -- Depth threshold for DFS.
+
+    Output:
+    a list of moves to reach to goal,
+    a score that counts the number of steps to reach goal,
+    a boolean value indicating whether there may be any remaining child nodes yet to be traversed.
+    """
+
+    # HERE: get all possible move from a given root
+
+    xCoords = [0, -1, -1, 0, 1, 1]
+    yCoords = [1, 1, 0, -1, -1, 0]
+
+    return (), INF, True
+
+
+def ids_score(state: dict[tuple, tuple], root: tuple) -> tuple:
+# def ids_score(state: dict[tuple, tuple], root: tuple) -> (int, [tuple]):
+    """
+    Depth-first limited search algorithm used for IDS. Searching up till a certain specified depth.
+
+    Arguments:
+    state -- The provided state of the board.
+    root  -- Start position.
+
+    Output:
+    the score, and a list of optimal moves to reach to goal (given specified root)
+    """
+    depth = 0
+    while True:
+        found, score, finished = dfs_limited(state, root, depth)
+        if found:
+            return found, score
+        elif finished:
+            return INF, ()
+        depth += 1
 
 
 def search(state: dict[tuple, tuple]) -> list[tuple]:
     """
-    This is the entry point for your submission. The input is a dictionary
-    of board cell states, where the keys are tuples of (r, q) coordinates, and
-    the values are tuples of (p, k) cell states. The output should be a list of 
-    actions, where each action is a tuple of (r, q, dr, dq) coordinates.
+    This is the entry point for your submission. The input is a dictionary of cell states, where
+    the keys are tuples of (r, q) coordinates, and the values are tuples of (p, k) cell states. The
+    output should be a list of  actions, where each action is a tuple of (r, q, dr, dq) coordinates.
 
     See the specification document for more details.
     """
 
-    # The render_board function is useful for debugging -- it will print out a 
-    # board state in a human-readable format. Try changing the ansi argument 
-    # to True to see a colour-coded version (if your terminal supports it).
+    # The render_board function is useful for debugging -- it will print out a board state in a
+    # human-readable format. Try changing the ansi argument to True to see a colour-coded version
+    # (if your terminal supports it).
     print(render_board(state, ansi=False))
 
-    # # CODE
-    # # get all moves
-    # all_roots = get_all_roots(state)
-    # min_score = 0
-    # for _ in all_roots:
-    #     # Do IDS to get the min score of each possible root
-    #     # get the min score and from that get ret (the best play to win)
-    #     min_score += 1
+    # CODE
+    # get all moves
+    all_roots = get_all_roots(state)
+    min_score = INF
+    min_ret = []
+    for root in all_roots:
+        # Do IDS (or informed searching algorithm) to get the min score of each possible root
+        # Get the min score and from that get ret (the best play to win)
+        score, ret = ids_score(state, root)
+        if score < min_score:
+            min_score = score
+            min_ret = ret
 
     # Here we're returning "hardcoded" actions for the given test.csv file.
     # Of course, you'll need to replace this with an actual solution...
-    return [
-        (5, 6, -1, 1),
-        (3, 1, 0, 1),
-        (3, 2, -1, 1),
-        (1, 4, 0, -1),
-        (1, 3, 0, -1)
-    ]
+    return min_ret
+
+# RAJA: HELPER FUNCTIONS
+
+# Main function to move pieces
+# For now returns state and a boolean indicating if it was updated
+def spread(position: tuple, direction:tuple, state: dict[tuple, tuple]) -> tuple:
+    # Check first if position in dictionary/is a red piece
+    if state[position][0] == 'b' or not position in state:
+        return state, False
+    
+    # Since a valid position to use, store POWER
+    currPower = state[position][1]
+    currPos = position
+
+    # Now decrement power and each time place
+    # a piece in a new position
+    while currPower != 0:
+        state, currPos = makeMove(currPos, direction, state)
+        currPower-=1
+    
+    # Last thing to do is remove the entry from the current posiion
+    del state[position]
+
+    # Now we want to move all the piecs from the stack to positions
+    # in the corresponding direction
+    return state, True
+
+# Returns the new position for iteration
+def makeMove(position: tuple, direction: tuple, state: dict[tuple, tuple]) -> tuple:
+    newPos = []
+    for i in range(len(position)):
+        currPos = position[i] + direction[i]
+        if 0 <= currPos <= 6:
+            newPos.append(currPos)
+        elif currPos == 7:
+            newPos.append(0)
+        elif currPos == -1:
+            newPos.append(6)
+    # Now check if there needs to be adjutment on the specific tile
+    newPos = (newPos[0], newPos[1])
+
+    if newPos in state:
+        newPower = state[newPos][1] + 1
+
+        # Here we want to remove the key from the board
+        if newPower > 6:
+            del state[newPos]
+        # Just increment power for used position
+        else:
+            state[newPos] = ('r', newPower)
+    
+    # Since this is new, can just add a single red piece
+    else:
+        state[newPos] = ('r', 1)
+
+    # Make sure to return the new position for the loop
+    return state, newPos
+
+
+# Test victory condition
+def checkVictory(state: dict[tuple, tuple]) -> bool:
+    for key in state.keys():
+        if state[key][0] == 'b':
+            return False
+    return True
---
+++ search/testSearch.py
@@ -0,0 +1,30 @@
+from utils import render_board
+from program import spread, makeMove
+
+xCoords = [0, -1, -1, 0, 1, 1]
+yCoords = [1, 1, 0, -1, -1, 0]
+
+def main():
+    board = {
+        (5, 6): ("r", 2),
+        (1, 0): ("b", 2),
+        (1, 1): ("b", 1),
+        (3, 2): ("b", 1),
+        (1, 3): ("b", 3),
+        (2, 0): ("r", 3),
+        (2, 4): ("b", 6),
+        (2, 5): ("b", 3)
+    }
+    print("OLD BOARD: ")
+    print(render_board(board))
+    # Put all possible edits here
+    # currDir = (xCoords[0], yCoords[1])
+    currDir = (xCoords[3], yCoords[3])
+    # board, _ = spread((5,6), currDir, board)
+    board, _ = spread((2,0), currDir, board)
+    print("NEW BOARD: ")
+    print(render_board(board))
+    return
+
+if __name__ == '__main__':
+    main()
