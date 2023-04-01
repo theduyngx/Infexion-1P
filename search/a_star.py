6f5d6d5f9d1583b995722bf975f9e3032d733b1f
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-04-01 12:04:33 +1100

    Adjusted h1 func from duy branch

--- search/a_star.py
+++ search/a_star.py
@@ -1,6 +1,13 @@
-# IMPORTS
+
+"""
+    Authors : The Duy Nguyen (1100548) and Ramon Javier L. Felipe VI (1233281)
+    File    : ids.py
+    Purpose : Informed search algorithm A* to find the optimal sequence of moves for a given input state
+              of a board to reach its goal state.
+"""
+
 import queue
-import heapq
+from .movement import spread
 from .program import check_victory, spread
 from .state import State, all_dir, MAX_VAL, PLAYER, ENEMY, SIZE
 from .dist_calculator import add_direction
@@ -9,247 +16,62 @@ from .dist_calculator import add_direction
 def A_star(board: dict[tuple, tuple]) -> list: #[tuple]:
     """
     A* algorithm to find the optimal sequence of moves to reach goal state
-
     @param board : the provided board (initial state)
     @return      : the sequence of optimal moves
     """
-    min_found = queue.PriorityQueue()  # discovered nodes
-    g_cost = {}  # real cumulative cost from root
-    f_cost = {}  # best guess f = g + h
-    discovered = {}
 
-    # TEST
-    num = 0
+    open_min   = queue.PriorityQueue()  # open set ordered by minimal cost first
+    g_cost     = {}                     # real cumulative cost from root
+    f_cost     = {}                     # best guess f = g + h
+    discovered = {}                     # open set - set of all generated but un-expanded nodes
 
-    curr_state = State(board, [], 0)
-    hash_curr = curr_state.__hash__()
-    min_found.put(curr_state)  # init state has now been discovered
+    curr_state = State(board, [], 0)    # current state initialized as init state
+    hash_curr  = curr_state.__hash__()  # hashed state for dictionary accessing
+    open_min.put(curr_state)            # init state has now been discovered
     discovered[hash_curr] = 1
-    g_cost[hash_curr] = 0
-    f_cost[hash_curr] = 0
-    # min_moves = []
-    # num_moves = INF
+    g_cost[hash_curr]     = 0           # real cost from init state
+    f_cost[hash_curr]     = 0           # f cost from init to goal state
 
-    while not min_found.empty():
-        curr_state = min_found.get()
+    # by using min-heap, we can immediately retrieve unexpanded nodes with lowest f-score
+    while not open_min.empty():
+        curr_state = open_min.get()
         hash_curr = curr_state.__hash__()
         del discovered[hash_curr]
-        if check_victory(curr_state.board):
-            print("Number of expansions =", num)
-            return curr_state.moves
-            # return the optimal moves to reach the goal state
-            # if len(curr_state.moves) < num_moves:
-            #     min_moves = curr_state.moves
-            #     num_moves = len(curr_state.moves)
 
+        # reached goal state
+        if check_victory(curr_state.board):
+            return curr_state.moves
+
+        # for each neighboring node (direct child) of current state
         for neighbor in get_neighbors(curr_state):
-            num+=1
             x, y, dir_x, dir_y = neighbor
             new_state = State.copy_state(curr_state)
             new_state.add_move(neighbor)
-            _ = spread(position=(x, y), direction=(dir_x, dir_y), board=new_state.board)
+
+            # each neighbor is a state resulted by single player move from current state
+            spread(position=(x, y),
+                   direction=(dir_x, dir_y),
+                   board=new_state.board)
             hash_new = new_state.__hash__()
+
             # true cost from init to new_state via curr_state
             g_cost_accum = g_cost[hash_curr] + 1
-
             if hash_new not in g_cost or g_cost_accum < g_cost[hash_new]:
                 g_cost[hash_new] = g_cost_accum
                 f_cost[hash_new] = g_cost_accum + h(new_state)
                 new_state.f_cost = f_cost[hash_new]
+
+                # update open sets
                 if hash_new not in discovered:
                     discovered[hash_new] = 1
-                    min_found.put(new_state)
-
-    print(num)
+                    open_min.put(new_state)
     return []
-
-
-def h(state: State) -> int:
-    """
-    The heuristic function.
-
-    @param state : given current state
-    @return      : the heuristic of said state (estimated number of moves to goal)
-    """
-    return h1(state)
-
-
-def h1(state: State) -> int:
-    total_heuristic = 0
-
-    # Get all the pieces
-    curr_board = state.board
-
-    # First store the pieces in the board from biggest
-    # to smallest
-    board_add = list(map(lambda tup: (tup[0], h_add(tup[1])), curr_board.items()))
-    enemies = [pos for pos in curr_board.keys() if curr_board[pos][0] == ENEMY]
-    sorted_board = sorted(board_add, key=lambda x: x[1][1], reverse=True)
-    captured_pieces = {}
-
-    # Now iterate through the list
-
-    for piece, _ in sorted_board:
-        if len(captured_pieces.keys()) >= len(enemies):
-            break
-        add, captured_pieces = h1_adjacent_blues(curr_board, piece, captured_pieces)
-        total_heuristic += add
-
-    # Once that is all done, check if there are any blues
-    # not_captured = len([key for key in captured_pieces.keys() if captured_pieces[key] == False])
-    not_captured = len(captured_pieces.keys()) == len(curr_board.keys())
-    total_heuristic += not_captured
-
-    return total_heuristic
-
-
-# Helper function for incrementing power in the board
-def h_add(cell: tuple) -> tuple:
-    tp, val = cell
-    if (tp == PLAYER):
-        print("Fuck")
-        if val < MAX_VAL:
-            val += 1
-    else:
-        val += 1
-    return tp, val
-
-
-# Helper funct for h1
-# Edited this adjacent blues for the new heuristic h1
-def h1_adjacent_blues(board: dict[tuple, tuple], piece: tuple, captured: dict) -> tuple:
-    _, curr_power = board[piece]
-    new_captures = {}
-    max_blues = 0
-
-    # Check all directions for the max
-    for dir in all_dir:
-        curr_blues = 0
-        new_pos = piece
-        curr_captures = {}
-        for _ in range(curr_power):
-            new_pos = add_direction(new_pos, dir)
-            if new_pos in board:
-                if board[new_pos][0] == ENEMY:
-                    curr_blues += new_pos not in captured
-                    curr_captures[new_pos] = True
-
-        # Need to adjust both our current max
-        # and the blues we will add to captured list
-        if curr_blues > max_blues:
-            max_blues = curr_blues
-            new_captures = curr_captures
-        del curr_captures
-
-    # Don't know how else to do this but add
-    # new_captures to captured list
-    # BETTER WAY TO IMPLEMENT THIS
-    # captured = captured + new_captures
-
-    # Max Blues being greater than 0 means a capture did occur
-    if max_blues > 0:
-        for item in new_captures.keys():
-            captured[item] = True
-        del new_captures
-        return 1, captured
-
-    del new_captures
-    return 0, captured
-
-
-def h2(state: State) -> int:
-    """
-    Heuristic function 2 - checking enemies in line (single direction) with no regards to the stack
-    value of the pieces.
-
-    @param state : given current state
-    @return      : the heuristic of said state (estimated number of moves to goal)
-    """
-
-    board = state.board
-    num_moves = 0
-    spreaded = {}
-    dict_dir = {}
-
-    # checks whether player piece is on the same direction as any direction with enemy piece
-    player_pieces = {}
-
-    for pos in board:
-        x, y = pos
-        tp, _ = board[pos]
-        if tp == PLAYER:
-            if pos not in player_pieces:
-                player_pieces[pos] = 0
-            continue
-
-        # initialize the piece entry in direction dictionary
-        dict_dir[(x, y, (1, 0))]  = []
-        dict_dir[(x, y, (0, 1))]  = []
-        dict_dir[(x, y, (1, -1))] = []
-
-        # for every other piece on the board - if player then player on direction
-        for _pos in board:
-            _x, _y = _pos
-            _tp, _ = board[_pos]
-            if _tp == PLAYER:
-                player_pieces[pos] = 1
-                continue
-
-            # x-direction
-            if _x == x and _y != y:
-                dict_dir[(x, y, (0, 1))].append(_pos)
-
-            # y-direction
-            elif _x != x and _y == y:
-                dict_dir[(x, y, (1, 0))].append(_pos)
-
-            # vertical direction
-            elif _x != x:
-                diff = abs(x - _x + y - _y)
-                if diff == 0 or diff == SIZE:
-                    dict_dir[(x, y, (1, -1))].append(_pos)
-
-        # delete empty enemy entries - implying that they do not share direction with any other pieces
-        # empty enemy entries also imply that they require at least 1 additional move to be spread on
-        empty = 0
-        if not dict_dir[(x, y, (0, 1))]:
-            del dict_dir[(x, y, (0, 1))]
-            empty += 1
-        if not dict_dir[(x, y, (1, 0))]:
-            del dict_dir[(x, y, (1, 0))]
-            empty += 1
-        if not dict_dir[(x, y, (1, -1))]:
-            del dict_dir[(x, y, (1, -1))]
-            empty += 1
-        num_moves += (empty == 3)
-
-    # whether any player piece is on same direction as any "enemy direction"
-    num_moves += not any(player_pieces.values())
-    del player_pieces
-
-    # then sort the direction dictionary by direction with the highest number of enemy pieces
-    dir_sort = list(map(lambda tup: (-len(tup[1]), tup[1]), dict_dir.items()))
-    del dict_dir
-
-    # due to it being min-heap (no max heap in Python3), we push negative lengths
-    found = False
-    heapq.heapify(dir_sort)
-    while dir_sort:
-        neg_len, pieces = heapq.heappop(dir_sort)
-        for piece in pieces:
-            found = piece not in spreaded
-            spreaded[piece] = 1
-        num_moves += found
-        del pieces
-
-    return num_moves
 
 
 def get_neighbors(state: State) -> list: #[tuple]:
     """
     Get all neighboring state of a given, current state of the board. Neighbors are all derived states
     resulted from player's single move.
-
     @param state : given current state
     @return      : list of all possible single move by player that returns a neighboring state
     """
@@ -262,3 +84,132 @@ def get_neighbors(state: State) -> list: #[tuple]:
         for dir_x, dir_y in all_dir:
             neighbors.append((x, y, dir_x, dir_y))
     return neighbors
+
+
+# ------------------------------------- HEURISTIC ----------------------------------------- #
+
+
+def piece_value_increment(cell: tuple) -> tuple:
+    """
+    For a given cell, which is a tuple in the form (position, piece), and piece is a tuple in the form
+    (piece type, stack value), increment its stack value.
+    @param cell : the provided cell;
+    @return     : cell with incremented stack value of piece on it
+    """
+    tp, val = cell
+    if tp == PLAYER:
+        val += val < MAX_VAL
+    else:
+        val += 1
+    return tp, val
+
+
+def enemy_filter(board: dict[tuple, tuple]) -> dict[tuple, tuple]:
+    """
+    Filter player pieces from the board, returning only a dictionary of enemies.
+    @param board : given board
+    @return      : the filtered board with only enemies
+    """
+    return {position: piece for position, piece in board.items() if piece[0] == ENEMY}
+
+
+def h(state: State) -> int:
+    """
+    Heuristic function: For every piece (from most stacked to least), we check the most number of enemies
+    that can be captured by it, regardless of the piece type (player or enemy). Once that is established,
+    all said enemies are considered captured and will be ignored by subsequent checks.
+    @param state : given current state
+    @return      : the heuristic of said state (estimated number of moves to goal)
+    """
+
+    board = state.board
+
+    # board_add increments 1 to pieces' stack: list of format (position=(x, y), piece=(type, value))
+    board_add    = list(map(lambda tup: (tup[0], piece_value_increment(tup[1])), board.items()))
+    sorted_board = dict(sorted(board_add, key=lambda tup: tup[1][1], reverse=True))
+    enemies      = enemy_filter(board)
+    num_moves    = 0
+    dict_dir     = {}
+
+    # from most stacked piece to least
+    for pos in sorted_board:
+        if enemies == {}:
+            break
+        x, y = pos
+        tp, val = sorted_board[pos]
+
+        # initialize the piece entry in direction dictionary
+        for dir in all_dir:
+            dict_dir[(x, y, dir)]  = []
+
+        # for every other piece on the board - if player then player on direction
+        for _pos in enemies:
+            _x, _y = _pos
+            _tp, _ = board[_pos]
+            x_diff = x - _x
+            y_diff = y - _y
+            xd_abs = abs(x_diff)
+            yd_abs = abs(y_diff)
+
+            # x-direction
+            if _x == x and _y != y:
+                sign = int(y_diff / yd_abs)
+
+                # add to both directions of x-direction if stack is sufficiently large
+                if yd_abs <= val:
+                    dict_dir[(x, y, (0, sign))].append(_pos)
+                if yd_abs <= SIZE - val:
+                    dict_dir[(x, y, (0, -sign))].append(_pos)
+
+            # y-direction
+            elif _x != x and _y == y:
+                sign = int(x_diff / xd_abs)
+
+                # add to both directions of y-direction if stack is sufficiently large
+                if xd_abs <= val:
+                    dict_dir[(x, y, (sign, 0))].append(_pos)
+                if xd_abs <= SIZE - val:
+                    dict_dir[(x, y, (-sign, 0))].append(_pos)
+
+            # vertical direction
+            elif _x != x:
+                diff = abs(x_diff + y_diff)
+                if xd_abs < yd_abs:
+                    x_sign = 1
+                    larger = yd_abs
+                else:
+                    x_sign = -1
+                    larger = xd_abs
+                y_sign = -x_sign
+
+                # add to both directions of vertical direction if stack is sufficiently large
+                if diff == 0 or diff == SIZE:
+                    dict_dir[(x, y, (x_sign, y_sign))].append(_pos)
+                    if larger <= val:
+                        dict_dir[(x, y, (-x_sign, -y_sign))].append(_pos)
+
+        # check the direction with most captures
+        max_captured = 0
+        captured = []
+        for dir in all_dir:
+            curr_len = len(dict_dir[(x, y, dir)])
+            if curr_len > max_captured:
+                max_captured = curr_len
+                captured = dict_dir[(x, y, dir)]
+
+        # if no capture
+        if not captured:
+            continue
+
+        # remove captured enemies from list of enemies left
+        for position in captured:
+            del enemies[position]
+        del captured
+        num_moves += 1
+
+    # cleanup
+    del dict_dir
+    del enemies
+    del sorted_board
+    del board_add
+    return num_moves
